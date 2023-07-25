394d9caa50f630429daab48c86f61ac28d66d908
genead <jirabona990@gmail.com>
2023-07-25 04:15:52 +0000

    done

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int largestValue = arr[0];
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > largestValue){
+                largestValue = arr[i];
+            }
+        }
+        return largestValue;
     }
 }
